5cbdcab2bba2bbc030d3a79a37be20c5281ed0a3
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-09 17:37:25 +0530

    updated code

--- main.tf
+++ main.tf
@@ -16,3 +16,10 @@ resource "aws_internet_gateway" "igw" {
     Name = "main"
   }
 }
+
+resource "aws_route" "igw" {
+  for_each                   = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
+  route_table_id             = each.value["id"]
+  destination_cidr_block     = "0.0.0.0/0"
+  gateway_id                 = aws_internet_gateway.igw.id
+}
