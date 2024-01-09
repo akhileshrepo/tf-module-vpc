1d21f40168d12e8b1b8ae6b925645b4804268b32
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-09 18:07:56 +0530

    updated code

--- main.tf
+++ main.tf
@@ -23,3 +23,14 @@ resource "aws_route" "igw" {
   destination_cidr_block     = "0.0.0.0/0"
   gateway_id                 = aws_internet_gateway.igw.id
 }
+
+resource "aws_eip" "ngw" {
+  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  allocation_id                  = lookup(lookup(aws_eip.ngw, each.key, null), "id", null)
+  subnet_id                      = each.value["id"]
+}
