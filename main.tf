8d277a82d26a8585bc3bba9c9ccf6a6bc6a142b5
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-09 17:14:10 +0530

    updated code

--- main.tf
+++ main.tf
@@ -8,3 +8,11 @@ module "subnets" {
   subnets = each.value
   vpc_id = aws_vpc.main.id
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
