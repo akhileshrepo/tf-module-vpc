3ae9fb8e784bd5b7d7667007f76e309c7c0f7fee
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-09 12:38:57 +0530

    updated code

--- vars.tf
+++ vars.tf
@@ -1,10 +1,2 @@
 variable "cidr" {}
 
-/*
-variable "subnets" {}
-variable "default_vpc_id" {}
-variable "default_vpc_cidr" {}
-variable "default_vpc_route_table_id" {}
-variable "tags" {}
-variable "env" {}
-*/
