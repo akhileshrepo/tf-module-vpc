bcdee7d3be31315ea2bfb4d3d4699e51fed31ccf
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-25 15:07:05 +0530

    updated code

--- subnets/main.tf
+++ subnets/main.tf
@@ -4,4 +4,22 @@ resource "aws_subnet" "main" {
   cidr_block = each.value["cidr"]
   availability_zone = each.value[ "az" ]
 
+  tags = {
+    Name = each.key
+  }
 }
+
+resource "aws_route_table" "main" {
+  for_each = var.subnets
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = lookup(lookup(aws_subnet.main, "each.key", null), "id", null)
+  route_table_id = lookup(aws_route_table.main, "each.key", null), "id", null)
+}
+
