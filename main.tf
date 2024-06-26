e033611ae0b601ced885deff04b317cba3a3f50e
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-26 10:35:29 +0530

    updated code

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_route" "igw" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
 }
-#
+
 # resource "aws_eip" "ngw" {
 #   count = length(local.public_subnet_ids)
 #   domain = "vpc"
