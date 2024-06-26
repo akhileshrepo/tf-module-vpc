5eac143ee1d22f9884a60809f4b4697746913c3e
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-26 11:43:43 +0530

    updated code

--- main.tf
+++ main.tf
@@ -26,15 +26,25 @@ resource "aws_route" "igw" {
 }
 
 resource "aws_eip" "ngw" {
-  for_each = lookup(lookup(module.subnets, "public", null), "subnets_ids", null)
+  count = length(local.public_subnet_ids)
   domain   = "vpc"
 }
 
 resource "aws_nat_gateway" "ngw" {
-  for_each = lookup(lookup(module.subnets, "public", null), "subnets_ids", null)
-  allocation_id = lookup(lookup(aws_eip.ngw, each.key, null), "id", null)
-  subnet_id     = each.value["id"]
+  count = length(local.public_subnet_ids)
+  allocation_id = element(aws_eip.ngw.*.id, count.index)
+  subnet_id     = element(local.public_subnet_ids, count.index)
 }
+
+resource "aws_route" "ngw" {
+  count                     = length(local.private_route_table_ids)
+  route_table_id            = element(local.private_route_table_ids, count.index)
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = element(aws_nat_gateway.ngw.*.id, count.index)
+}
+
+
+
 
 output "subnets" {
   value = module.subnets
