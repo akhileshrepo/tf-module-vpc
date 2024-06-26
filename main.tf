49e234272cfe294fb9a0589ed2040e30bff364d2
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-26 14:29:51 +0530

    updated code

--- main.tf
+++ main.tf
@@ -66,6 +66,7 @@ resource "aws_instance" "main" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id = local.app_subnet_ids[0]
 }
 
 resource "aws_security_group" "allow_tls" {
