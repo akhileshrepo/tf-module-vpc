eec56bbafb1cc3c6f804a7c67923961b83f960bc
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-26 11:44:25 +0530

    updated code

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_route" "ngw" {
   count                     = length(local.private_route_table_ids)
   route_table_id            = element(local.private_route_table_ids, count.index)
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                = element(aws_nat_gateway.ngw.*.id, count.index)
+  nat_gateway_id                = element(aws_nat_gateway.ngw.*.id, count.index)
 }
 
 
