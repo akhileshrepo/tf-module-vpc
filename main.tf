f8c9616d793896e6d249040588a9df2501ce4128
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-26 14:28:39 +0530

    updated code

--- main.tf
+++ main.tf
@@ -62,9 +62,36 @@ resource "aws_route" "default-vpc-peer-entry" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
-
-
-
-output "subnets" {
-  value = module.subnets
+resource "aws_instance" "main" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow from TLS"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
