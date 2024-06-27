7c55166e0d080ec311def00f215a9b6d94a097c4
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-28 02:46:59 +0530

    updated code

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_route" "igw" {
-  for_each = lookup(lookup(module.subnets, "public", null), "aws_route_table", null)
-  route_table_id         = each.value[id]
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
+# resource "aws_route" "igw" {
+#   for_each = lookup(lookup(module.subnets, "public", null), "aws_route_table", null)
+#   route_table_id         = each.value[id]
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.igw.id
+# }
 
