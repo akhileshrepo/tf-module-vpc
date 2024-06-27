c5b98e8955fc25f063df060df1d3e7dbfe4153ba
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-28 01:59:48 +0530

    updated code

--- subnets/main.tf
+++ subnets/main.tf
@@ -10,8 +10,16 @@ resource "aws_route_table" "main" {
   vpc_id = var.vpc_id
 }
 
-resource "aws_route_table_association" "a" {
-  for_each = var.subnets
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
+# resource "aws_route_table_association" "a" {
+#   for_each = var.subnets
+#   subnet_id      = aws_subnet.main.id
+#   route_table_id = aws_route_table.main.id
+# }
+
+output "subnet_id" {
+  value = aws_subnet.main
+}
+
+output "route_table_id" {
+  value = aws_route_table.main
 }
