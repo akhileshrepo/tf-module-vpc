41147d1d5f9353b331c99097b1aab64e1ab300b3
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-28 02:32:46 +0530

    updated code

--- subnets/main.tf
+++ subnets/main.tf
@@ -16,10 +16,3 @@ resource "aws_route_table_association" "main" {
   route_table_id = lookup(lookup(aws_route_table.main, each.key, null), "id", null)
 }
 
-output "route_table" {
-  value = aws_route_table.main
-}
-
-output "aws_subnet" {
-  value = aws_subnet.main
-}
