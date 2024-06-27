139ec06e941006771f245b3d584e99c07d69cd4f
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-27 12:28:01 +0530

    updated code

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_route" "peering" {
 }
 
 resource "aws_route" "default-vpc-peer-entry" {
-  route_table_id            = var.default_route_table_id
+  route_table_id            = var.default_vpc_route_table_id
   destination_cidr_block    = var.cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
--- vars.tf
+++ vars.tf
@@ -2,6 +2,6 @@ variable "cidr" {}
 variable "subnets" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
-variable "default_route_table_id" {}
+variable "default_vpc_route_table_id" {}
 variable "tags" {}
 variable "env" {}
