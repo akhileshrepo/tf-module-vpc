80aa949044bc0411b3cffa61fc6459835a8a5897
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-29 14:26:51 +0530

    updated code

--- main.tf
+++ main.tf
@@ -21,3 +21,39 @@ resource "aws_route" "igw" {
   gateway_id             = aws_internet_gateway.igw.id
 }
 
+resource "aws_eip" "ngw" {
+  count = length(local.public_subnet_ids)
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count = length(local.public_subnet_ids)
+  allocation_id = element(aws_eip.ngw.*.id, count.index)
+  subnet_id = element(local.public_subnet_ids, count.index)
+}
+
+resource "aws_route" "ngw" {
+  count                  = length(local.private_route_table_ids)
+  route_table_id         = element(local.private_route_table_ids, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
+}
+
+resource "aws_vpc_peering_connection" "peering" {
+  peer_vpc_id = aws_vpc.main.id
+  vpc_id      = var.default_vpc_id
+  auto_accept = true
+}
+
+resource "aws_route" "peer" {
+  count                     = length(local.private_route_table_ids)
+  route_table_id            = element(local.private_route_table_ids, count.index)
+  destination_cidr_block    = var.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
+
+resource "aws_route" "default-vpc-peer-entry" {
+  route_table_id            = var.default_vpc_route_table_id
+  destination_cidr_block    = var.cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
